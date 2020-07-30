dcc2c2d622c5f5e4561d1f260c4b0be6657d5434
樊乘乘 <fcscanf@outlook.com>
2020-07-30 15:26:10 +0800

    Add-注册中心创建完成

---
+++ register_center/src/test/java/com/fcant/register_center/RegisterCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcant.register_center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
